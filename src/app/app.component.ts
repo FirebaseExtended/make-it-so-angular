d7eac43734f86c75c379fccd1b3a6185f6723243
Lingkai Shen <lingkai@google.com>
2024-08-27 15:34:56 -0400

    Don't use take(1) so new tasks are refreshed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { TaskWithSubtasks, Task, TaskService } from './services/task.service';
-import { catchError, take, tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { GoogleGenerativeAIFetchError } from '@google/generative-ai';
 
@@ -211,7 +211,6 @@ export class AppComponent {
         });
         return [];
       }),
-      take(1),
     );
   }
 
